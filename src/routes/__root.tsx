9346184a9928964d225a03888cd08f7e362eff01
tigawanna <denniskinuthiaw@gmail.com>
2024-09-25 11:41:25 +0300

    add css view transitions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
 import {createRootRouteWithContext } from '@tanstack/react-router'
-import "./angled-transition.css"
-import "./wipe-transition.css"
-import "./slides-transition.css"
-import "./flip-transition.css"
-import "./vertical-transition.css"
+import "@/view-transition/angled-transition.css"
+import "@/view-transition/wipe-transition.css"
+import "@/view-transition/slides-transition.css"
+import "@/view-transition/flip-transition.css"
+import "@/view-transition/vertical-transition.css"
 import "./styles.css"
 import { PocketBaseClient } from '@/lib/pb/client';
 import { QueryClient } from '@tanstack/react-query';
