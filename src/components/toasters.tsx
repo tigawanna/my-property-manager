e5415361380f97451d06f45083306cba0f4c9842
tigawanna <denniskinuthiaw@gmail.com>
2024-09-25 12:10:03 +0300

    add toast messages

--- src/components/toasters.tsx
+++ src/components/toasters.tsx
@@ -1,10 +1,11 @@
-import { CheckCircle2 } from 'lucide-react';
+import { CheckCircle2, CircleAlert, CircleX } from 'lucide-react';
 import toast,{ToastOptions} from 'react-hot-toast';
 
 interface ToasterProps extends ToastOptions {
   title:string;
   message: string;
   duration?: number;
+  variant:"success"|"error"|"warning"|"info";
 }
 export function successToast({title,message,...props}: ToasterProps) {
 return toast.custom((t) => {
@@ -12,12 +13,121 @@ return toast.custom((t) => {
     <div
       className={`${
         t.visible ? "animate-enter" : "animate-leave"
-      } pointer-events-auto flex w-full max-w-md rounded-lg bg-success-content  text-success shadow-lg shadow-success ring-1 ring-success ring-opacity-5 dark:shadow-sm`}
+      } pointer-events-auto flex w-full max-w-md rounded-lg bg-base-300  text-success shadow-lg shadow-success ring-1 ring-success ring-opacity-5 dark:shadow-sm`}
     >
       <div className="w-0 flex-1 p-4">
         <div className="flex justify-center items-center ">
           <div className="h-full flex-shrink-0 items-center justify-center pt-0.5">
             <CheckCircle2 className="aspect-square h-full text-success" />
+          </div>
+          <div className="ml-3 flex-1">
+            <p className="font-medium">{title}</p>
+            <p className="mt-1 text-sm">{message}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border-l border-success">
+        <button
+          onClick={() => toast.dismiss(t.id)}
+          className="btn btn-ghost h-full w-full"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );},{...props,position:"bottom-left"})
+};
+export function errorToast({title,message,...props}: ToasterProps) {
+return toast.custom((t) => {
+  return (
+    <div
+      className={`${
+        t.visible ? "animate-enter" : "animate-leave"
+      } pointer-events-auto flex w-full max-w-md rounded-lg bg-base-300  text-success shadow-lg shadow-success ring-1 ring-success ring-opacity-5 dark:shadow-sm`}
+    >
+      <div className="w-0 flex-1 p-4">
+        <div className="flex justify-center items-center ">
+          <div className="h-full flex-shrink-0 items-center justify-center pt-0.5">
+            <CheckCircle2 className="aspect-square h-full text-success" />
+          </div>
+          <div className="ml-3 flex-1">
+            <p className="font-medium">{title}</p>
+            <p className="mt-1 text-sm">{message}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border-l border-success">
+        <button
+          onClick={() => toast.dismiss(t.id)}
+          className="btn btn-ghost h-full w-full"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );},{...props,position:"bottom-left"})
+};
+export function infoToast({title,message,...props}: ToasterProps) {
+return toast.custom((t) => {
+  return (
+    <div
+      className={`${
+        t.visible ? "animate-enter" : "animate-leave"
+      } pointer-events-auto flex w-full max-w-md rounded-lg bg-base-300  text-success shadow-lg shadow-success ring-1 ring-success ring-opacity-5 dark:shadow-sm`}
+    >
+      <div className="w-0 flex-1 p-4">
+        <div className="flex justify-center items-center ">
+          <div className="h-full flex-shrink-0 items-center justify-center pt-0.5">
+            <CheckCircle2 className="aspect-square h-full text-success" />
+          </div>
+          <div className="ml-3 flex-1">
+            <p className="font-medium">{title}</p>
+            <p className="mt-1 text-sm">{message}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border-l border-success">
+        <button
+          onClick={() => toast.dismiss(t.id)}
+          className="btn btn-ghost h-full w-full"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );},{...props,position:"bottom-left"})
+};
+export function makeHotToast({title,message,variant="info",...props}: ToasterProps) {
+  const toastVariantStyle = (toastAvriant: typeof variant) => {
+    switch (toastAvriant) {
+      case "success":
+        return "text-success";
+       case "info":
+        return "text-info";
+      case "warning":
+        return "text-warning";
+      case "error":
+        return "text-error";
+        default:
+          return "text-info"; 
+    }
+  };
+return toast.custom((t) => {
+  return (
+    <div
+      className={`${
+        t.visible ? "animate-enter" : "animate-leave"
+      } 
+        ${toastVariantStyle(variant)}
+        pointer-events-auto flex w-full max-w-md rounded-lg bg-base-300   shadow-lg shadow-success ring-1 ring-success ring-opacity-5 dark:shadow-sm`}
+    >
+      <div className="w-0 flex-1 p-4">
+        <div className="flex justify-center items-center ">
+          <div className="h-full flex-shrink-0 items-center justify-center pt-0.5">
+            {variant === "success" && <CheckCircle2 className="aspect-square h-full text-success" />}
+            {variant === "info" && <CircleAlert  className="aspect-square h-full text-info" />}
+            {variant === "warning" && <CircleAlert className="aspect-square h-full text-warning" />}
+            {variant === "error" && <CircleX  className="aspect-square h-full text-error" />}
           </div>
           <div className="ml-3 flex-1">
             <p className="font-medium">{title}</p>
--- src/routes/-components/HomePage.tsx
+++ src/routes/-components/HomePage.tsx
@@ -1,18 +1,24 @@
-import { successToast } from "@/components/toasters";
+import { makeHotToast, successToast } from "@/components/toasters";
 import { useState } from "react";
 
 export function HomePage(){
-    const [toastcount, setToastcount] = useState(1)
+const [toastcount, setToastcount] = useState(1)
 return (
   <div className="flex h-full w-full flex-col items-center justify-center">
     <h3 className="text-2xl font-bold">Welcome Home!</h3>
     <button className="btn btn-success" onClick={() => {
+      makeHotToast({title:"success toast",message:"success toast",variant:"success"})
+    }}>success toast</button>
 
-    successToast({
-      title: `Toast ${toastcount}`,
-      message: "This is a success toast",
-    })
-    setToastcount(toastcount + 1)
+    <button className="btn btn-info" onClick={() => {
+      makeHotToast({title:"info toast",message:"info toast",variant:"info"})
+    }}>success toast</button>
+
+    <button className="btn btn-error" onClick={() => {
+      makeHotToast({title:"error toast",message:"error toast",variant:"error"})
+    }}>success toast</button>
+    <button className="btn btn-warning" onClick={() => {
+      makeHotToast({title:"warning toast",message:"warning toast",variant:"warning"})
     }}>success toast</button>
   </div>
 );
