5e639cf2a8524a546f75c757f34952ad3fb207af
tigawanna <denniskinuthiawaweru@gmail.com>
2024-09-29 20:29:07 +0300

    more error boundaries

--- src/routes/dashboard/bills/print.tsx
+++ src/routes/dashboard/bills/print.tsx
@@ -2,16 +2,18 @@ import { authGuard } from '@/lib/tanstack/query/use-viewer'
 import { createFileRoute } from '@tanstack/react-router'
 import { z } from 'zod'
 import PrintBillsContainerBills from './-components/print/PrintBillsContainer'
+import { RouterErrorComponent } from '@/lib/tanstack/router/routerErrorComponent'
 const searchparams = z.object({
   cy: z.number().optional(),
   cm: z.number().optional(),
   py: z.number().optional(),
   pm: z.number().optional(),
 })
-export const Route = createFileRoute('/dashboard/bills/print')({
+export const Route = createFileRoute("/dashboard/bills/print")({
   component: PrintBillsContainerBills,
   validateSearch: (search) => searchparams.parse(search),
+  errorComponent: ({ error }) => <RouterErrorComponent error={error} />,
   async beforeLoad(ctx) {
-    await authGuard({ ctx })
+    await authGuard({ ctx });
   },
-})
+});
