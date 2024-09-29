eb03df495b9140e5d5340da9e83d1ebe40fff4ec
tigawanna <denniskinuthiawaweru@gmail.com>
2024-09-29 19:47:57 +0300

    mek text ereadavle

--- src/lib/tanstack/router/routerErrorComponent.tsx
+++ src/lib/tanstack/router/routerErrorComponent.tsx
@@ -5,7 +5,7 @@ interface RouterErrorComponentProps {
 export function RouterErrorComponent({ error }: RouterErrorComponentProps) {
   return (
     <div className="flex h-full min-h-screen w-full flex-col items-center justify-center">
-      <div className="rounded-xl border border-error bg-error/20 p-5 text-error-content">
+      <div className="rounded-xl border border-error bg-error/20 p-5 ">
         <p className="">{error.name}</p>
         <p className="text-sm">{error.message}</p>
       </div>
