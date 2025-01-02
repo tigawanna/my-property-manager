6787eafb0ffdb64575e3ea661daa5194b4e1a248
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-02 14:16:34 +0300

    feat: implement admin authentication and tenant impersonation in PocketBase client tests

--- src/lib/pb/tests/test-pb-client.ts
+++ src/lib/pb/tests/test-pb-client.ts
@@ -2,6 +2,35 @@ import { TypedPocketBase, } from "@tigawanna/typed-pocketbase";
 import { LocalAuthStore } from "pocketbase";
 import { Schema } from "../pb-types";
 
-export type PocketBaseClient = TypedPocketBase<Schema>;
+
+
+async function adminPB() {
+  if (
+    !process.env.PB_ADMIN_EMAIL ||
+    !process.env.PB_ADMIN_PASSWORD ||
+    !process.env.VITE_PB_URL
+  ) {
+    throw new Error(
+      "Please provide the admin email and password in the .env file",
+    );
+  }
 const PB_URL = import.meta.env.VITE_PB_URL;
-export const test_pb = new TypedPocketBase<Schema>(PB_URL, new LocalAuthStore("test_token"));
+const test_pb = new TypedPocketBase<Schema>(
+  PB_URL,
+  new LocalAuthStore("test_token"),
+);
+
+  await test_pb
+    .from("_superusers")
+    .authWithPassword(
+      process.env.PB_ADMIN_EMAIL,
+      process.env.PB_ADMIN_PASSWORD,
+    );
+  return test_pb;
+}
+
+export async function impersonateTenant() {
+const pb = await adminPB();
+// const tenanatPB = pb.impersonate()
+ await pb.from("_superusers").authWithPassword("admin", "password");
+}
