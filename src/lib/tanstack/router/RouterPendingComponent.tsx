d3326788d2d90b7c0a113b77e94f99715a85df9c
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-04 19:58:23 +0300

    feat: add SiteIcon component and update RouterPendingComponent to include it

--- src/lib/tanstack/router/RouterPendingComponent.tsx
+++ src/lib/tanstack/router/RouterPendingComponent.tsx
@@ -1,13 +1,13 @@
+import SiteIcon from "@/components/icons/Siteicon";
+
 interface RouterPendingComponentProps {}
 
 export function RouterPendingComponent({}: RouterPendingComponentProps) {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-2">
-      <div className="skeleton h-[80vh] w-[95%] rounded-2xl bg-base-300" />
-      {/* <div className="justify-center' flex w-full flex-col items-center md:flex-row gap-3">
-        <div className="skeleton h-[40vh] w-full rounded-lg bg-base-300" />
-        <div className="skeleton h-[40vh] w-full rounded-lg bg-base-300" />
-      </div> */}
+      <div className="skeleton h-[80vh] w-[95%] flex justify-center items-center rounded-2xl bg-base-300/30" >
+      <SiteIcon height={250}/>
+      </div>
     </div>
   );
 }
